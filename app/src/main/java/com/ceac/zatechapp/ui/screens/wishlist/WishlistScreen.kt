946c55bc1dd408cfe6f63d3137e90a100062c9db
Juan Giraldo <juanjogb300@gmail.com>
2025-11-10 21:56:41 +0100

    Actualización lista de deseos

--- app/src/main/java/com/ceac/zatechapp/ui/screens/wishlist/WishlistScreen.kt
+++ app/src/main/java/com/ceac/zatechapp/ui/screens/wishlist/WishlistScreen.kt
@@ -1,22 +1,31 @@
 package com.ceac.zatechapp.ui.screens.wishlist
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -25,22 +34,23 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.ceac.zatechapp.ui.screens.auth.ForgotPasswordLinkVisual
-import com.ceac.zatechapp.ui.screens.auth.LoginButtonVisual
-import com.ceac.zatechapp.ui.screens.auth.LoginFieldsVisual
-import com.ceac.zatechapp.ui.screens.auth.LoginHeaderVisual
-import com.ceac.zatechapp.ui.screens.auth.LoginIconsVisual
-import com.ceac.zatechapp.ui.screens.auth.RegisterLinkVisual
-import com.ceac.zatechapp.ui.screens.auth.ResetPasswordLinkVisual
+import com.ceac.zatechapp.R
 import com.ceac.zatechapp.ui.screens.home.AppBottomBarVisual
 import com.ceac.zatechapp.ui.theme.BackgroundWhite
 import com.ceac.zatechapp.ui.theme.BlackPrimaryText
+import com.ceac.zatechapp.ui.theme.GraySecondaryText
+import com.ceac.zatechapp.ui.theme.PrimaryRed
+import com.ceac.zatechapp.ui.theme.SecondaryGreen
 
 
 @Composable
@@ -60,7 +70,6 @@ fun WishlistScreenVisual() {
 
                 TextTop()
 
-
                 // 1. Barra Superior (Búsqueda)
 
                 TopSearchWishlist(
@@ -68,7 +77,13 @@ fun WishlistScreenVisual() {
                     placeholder = "Search"
                 )
 
-                Spacer(modifier = Modifier.height(10.dp))
+                Spacer(modifier = Modifier.height(30.dp))
+
+                CardsProduct(
+                    productName = "Zapatillas para caminatas",
+                    productPrice = "80,00 EUR",
+                    image = R.drawable.zapatillas_nike
+                )
 
                 // Espacio extra al final para asegurar que el último producto no quede pegado al Bottom Bar
                 Spacer(modifier = Modifier.height(80.dp))
@@ -83,7 +98,8 @@ fun WishlistScreenVisual() {
 @Composable
 fun TextTop() {
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally) {
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
         Text(
             text = "Lista de deseos",
@@ -150,6 +166,108 @@ fun TopSearchWishlist(
         }
     }
 }
+
+
+@Composable
+fun CardsProduct(
+    productName: String,
+    productPrice: String,
+    image: Int
+) {
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(12.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFE5E5E5)),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        //ORGANIZADOR PRINCIPAL: Columna (arriba-abajo)
+        Column(modifier = Modifier.padding(12.dp)) {
+
+            //ORGANIZADOR CABECERA: Fila (izquierda-derecha)
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                // Imagen circular del producto
+                Image(
+
+                    painter = painterResource(id = image),
+                    contentDescription = productName,
+                    modifier = Modifier
+                        .size(70.dp)
+                        .clip(CircleShape)
+                        .border(2.dp, Color(0xFFE74C3C), CircleShape) // borde rojo
+                )
+
+                Spacer(modifier = Modifier.width(12.dp))
+
+                // Columna con nombre y precio
+                Column {
+                    Text(
+                        text = productName,
+                        fontSize = 14.sp,
+                        color = Color.Black,
+                        fontWeight = FontWeight.Normal
+                    )
+                    Text(
+                        text = productPrice,
+                        fontSize = 16.sp,
+                        color = SecondaryGreen, // verde oscuro
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                // Icono eliminar
+                IconButton(
+                    onClick = {/* */ },
+                    colors = IconButtonDefaults.iconButtonColors(
+                        containerColor = Color(0xFFFFD3D3).copy(alpha = 1f)
+                    ),
+                    modifier = Modifier
+                        .size(40.dp)
+
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = "Eliminar",
+                        tint = Color(0xFFE74C3C) // rojo
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                // Botón de añadir a la cesta
+                Button(
+                    onClick = {/* */ },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFEA553B)),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(40.dp),
+                    shape = RoundedCornerShape(10.dp)
+                ) {
+                    Text(
+                        text = "AÑADIR A LA CESTA",
+                        color = Color.White,
+                        fontWeight = FontWeight.ExtraBold,
+                        fontStyle = FontStyle.Italic,
+                        fontSize = 13.sp
+                    )
+                }
+            }
+
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
